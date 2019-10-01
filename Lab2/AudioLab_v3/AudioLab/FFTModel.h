84619ea63b868f640ba02ad6c0c339c25e71ba75
ericouyang <oyzhenxuan@outlook.com>
2019-10-01 12:50:43 -0500

    new audio project

---
+++ Lab2/AudioLab_v3/AudioLab/FFTModel.h
@@ -0,0 +1,28 @@
+//
+//  FFTModel.h
+//  AudioLab
+//
+//  Created by 梅沈潇 on 9/30/19.
+//  Copyright © 2019 Eric Larson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#define BUFFER_SIZE 2048*4
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFTModel : NSObject
+
++(FFTModel*) sharedInstance;
+
+-(void) start;
+
+-(float*) fftData;
+
+-(float*) getFrequencies;
+
+-(void) clear;
+
+@end
+
+NS_ASSUME_NONNULL_END
