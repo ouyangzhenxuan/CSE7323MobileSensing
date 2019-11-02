b8f361a1b6e7960a96bc9a36810ced4d86e986d0
ericouyang <oyzhenxuan@outlook.com>
2019-11-02 15:52:48 -0500

    final

--- Lab4/ImageLab-moduleB/Pods/Target Support Files/Pods-ImageLab/Pods-ImageLab-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImageLab : NSObject
-@end
-@implementation PodsDummy_Pods_ImageLab
-@end
--- Lab4/ImageLab-moduleB/Pods/Target Support Files/Pods-ImageLabTests/Pods-ImageLabTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImageLabTests : NSObject
-@end
-@implementation PodsDummy_Pods_ImageLabTests
-@end
